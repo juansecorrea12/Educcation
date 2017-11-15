a44c5e9983f2f989244ed6ea986169aeed8b8c25
juansecorrea12 <juansecorrea12@gmail.com>
2017-11-14 20:42:20 -0500

    Add files via upload

---
+++ Representación virtual/Phase02.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Phase02 : MonoBehaviour {
+
+	public GameObject C;
+	public GameObject I;
+
+	private int Cont;
+	private bool ValR1;
+	private bool ValR2;
+	private bool ValR3;
+	private bool ValR4;
+	private bool ValR5;
+	private bool ValR6;
+	private bool ValR7;
+
+	void Start () {
+		Cont = 0;
+		ValR1 = true;
+		ValR2 = true;
+		ValR3 = true;
+		ValR4 = true;
+		ValR5 = true;
+		ValR6 = true;
+		ValR7 = true;
+	}
+
+	void Update () {
+		if (Ctrl.CtrlMain.R1 == "R1a" && Ctrl.CtrlMain.RF == "RFa" && ValR1) {
+			ValR1 = false;
+			StartCoroutine (Correct ());
+		}
+		if (Ctrl.CtrlMain.R2 == "R2a" && Ctrl.CtrlMain.RF == "RFa" && ValR2) {
+			ValR2 = false;
+			StartCoroutine (Correct ());
+		}
+		if (Ctrl.CtrlMain.R3 == "R3a" && Ctrl.CtrlMain.RNF == "RNFa" && ValR3) {
+			ValR3 = false;
+			StartCoroutine (Correct ());
+		}
+		if (Ctrl.CtrlMain.R4 == "R4a" && Ctrl.CtrlMain.RF == "RFa" && ValR4) {
+			ValR4 = false;
+			StartCoroutine (Correct ());
+		}
+		if (Ctrl.CtrlMain.R5 == "R5a" && Ctrl.CtrlMain.RNF == "RNFa" && ValR5) {
+			ValR5 = false;
+			StartCoroutine (Correct ());
+		}
+		if (Ctrl.CtrlMain.R6 == "R6a" && Ctrl.CtrlMain.RNF == "RNFa" && ValR6) {
+			ValR6 = false;
+			StartCoroutine (Correct ());
+		}
+		if (Ctrl.CtrlMain.R7 == "R7a" && Ctrl.CtrlMain.RF == "RFa" && ValR7) {
+			ValR7 = false;
+			StartCoroutine (Correct ());
+		}
+		if (Ctrl.CtrlMain.R1 == "R1a" && Ctrl.CtrlMain.RNF == "RNFa")
+			StartCoroutine(Incorrect());
+		if (Ctrl.CtrlMain.R2 == "R2a" && Ctrl.CtrlMain.RNF == "RNFa")
+			StartCoroutine(Incorrect());
+		if (Ctrl.CtrlMain.R3 == "R3a" && Ctrl.CtrlMain.RF == "RFa")
+			StartCoroutine(Incorrect());
+		if (Ctrl.CtrlMain.R4 == "R4a" && Ctrl.CtrlMain.RNF == "RNFa")
+			StartCoroutine(Incorrect());
+		if (Ctrl.CtrlMain.R5 == "R5a" && Ctrl.CtrlMain.RF == "RFa")
+			StartCoroutine(Incorrect());
+		if (Ctrl.CtrlMain.R6 == "R6a" && Ctrl.CtrlMain.RF == "RFa")
+			StartCoroutine(Incorrect());
+		if (Ctrl.CtrlMain.R7 == "R7a" && Ctrl.CtrlMain.RNF == "RNFa")
+			StartCoroutine(Incorrect());
+
+		if (Cont == 2) {
+			SceneManager.LoadScene(2);
+		}
+	}
+
+	IEnumerator Correct () {
+		Cont += 1;
+		C.SetActive (true);
+		yield return new WaitForSeconds(3f);
+		C.SetActive (false);
+	}
+	IEnumerator Incorrect () {
+		I.SetActive (true);
+		yield return new WaitForSeconds(3f);
+		I.SetActive (false);
+	}
+}
---
+++ Representación virtual/Phase03.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Phase03 : MonoBehaviour {
+
+	public GameObject C;
+	public GameObject I;
+	public GameObject E;
+	private bool Val1 = true;
+	private bool Val2 = true;
+	private bool Val3 = true;
+	
+	void Update () {
+		if (Ctrl.CtrlMain.RGB == "R" && Val1) {
+			Val1 = false;
+			StartCoroutine (Incorrect ());
+		}
+		if (Ctrl.CtrlMain.RGB == "V" && Val2) {
+			Val2 = false;
+			StartCoroutine (Incorrect ());
+		}
+		if (Ctrl.CtrlMain.RGB == "G" && Val3) {
+			Val3 = false;
+			StartCoroutine (Correct ());
+		}
+	}
+
+	IEnumerator Correct () {
+		C.SetActive (true);
+		yield return new WaitForSeconds(3f);
+		C.SetActive (false);
+		E.SetActive (true);
+		yield return new WaitForSeconds(2f);
+		Application.Quit ();
+	}
+	IEnumerator Incorrect () {
+		I.SetActive (true);
+		yield return new WaitForSeconds(3f);
+		I.SetActive (false);
+	}
+}
